105a68a30def9323c12280b502a62d170b091caa
Rafail Abdyushev <rafaelab@mail.ru>
2023-06-26 17:58:23 +0300

    media screens

--- styled/front/src/App.js
+++ styled/front/src/App.js
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
-import {Frame, Day, Header, Button, Body, Column, WorkBlock, Hours, BodyWork, FooterBlock} from "./StComponents"
+import {Frame, Day, Header, Button, Body, Column, WorkBlock, Hours, BodyWork, FooterBlock, HeaderBlock, Months} from "./StComponents"
 import Workarea from './Workarea';
 
 function App() {
@@ -8,13 +8,10 @@ function App() {
   const DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
   const DAYS_OF_THE_WEEK = ['M', 'T', 'W', 'T', 'F', 'S', 'S'];
-  const MONTHS = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+  const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September',
+                           'October', 'November', 'December'];
 
-  function isLeapYear(year) {
-    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-  }
-
-  const days = isLeapYear(year) ? DAYS_LEAP : DAYS;
+  
 
   
   const today = new Date();
@@ -50,6 +47,12 @@ function App() {
     return newArr3;
   }
 
+  function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  }
+
+  const days = isLeapYear(year) ? DAYS_LEAP : DAYS;
+
   function getStartDayOfMonth(date) {
     const startDate = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
     return startDate === 0 ? 7 : startDate;
@@ -77,38 +80,40 @@ function App() {
 
   const deleteInterview = (indexToDelete) => {
     childrens[indexToDelete].style.backgroundColor = "transparent";
+    deleteRef.current.style.display = 'none';
+    setInterviews(interviews.filter(item => item !== indexToDelete));
   }
 
   return (
     <Frame>
-      <Header initial ref ={wrapperRef}>
-        <div>Interview Calendar</div>
-        <div onClick={plusButton}>+</div>
+      <Header key={'1'} initial ref ={wrapperRef}>
+        <HeaderBlock key={'1'}>Interview Calendar</HeaderBlock>
+        <HeaderBlock plus key={'2'} onClick={plusButton}>+</HeaderBlock>
       </Header>
-      <Header>
+      <Header key={'2'}>
         <Column></Column>
         <Body initial>
           {DAYS_OF_THE_WEEK.map((d) => (
-            <Day key={d}>
+            <Day weekday key={d.toString()}>
               {d}
             </Day>
           ))}
         </Body>
       </Header>
-      <Header>
+      <Header key={'3'}>
         <Column></Column>
         <Body initial>
           {week.map(i => {
                   const todayCheck = new Date();
                   if ((i == todayCheck.getDate() && month == todayCheck.getMonth())) {
                     return (
-                      <Day today key={i}>
+                      <Day today key={i.toString()}>
                         <strong>{i}</strong>
                       </Day>
                     );
                   } else {
                     return (
-                      <Day key={i}>
+                      <Day key={i.toString()}>
                         <strong>{i}</strong>
                       </Day>
                     );
@@ -116,20 +121,20 @@ function App() {
           })}
         </Body>
       </Header>
-      <Header>
+      <Header key={'4'}>
         <Column></Column>
         <Body initial>
-          <Button onClick={() => setDate(new Date(year, month, day - 7))}>{'<'}</Button>
-          <div>
+          <Button key={'1'} onClick={() => setDate(new Date(year, month, day - 7))}>{'<'}</Button>
+          <Months>
             {MONTHS[month]} {year}
-          </div>
-          <Button onClick={() => setDate(new Date(year, month, day + 7))}>{'>'}</Button>
+          </Months>
+          <Button key={'2'} onClick={() => setDate(new Date(year, month, day + 7))}>{'>'}</Button>
         </Body>
       </Header>
       <Workarea giveRef={wrapperRef} workBgrnd={workBgrnd} data={interviews}></Workarea>
-      <Header initial>
-        <FooterBlock>Today</FooterBlock>
-        <FooterBlock deleteBtn ref={deleteRef} onClick={() => deleteInterview(interwiewToDelete)}>Delete</FooterBlock>
+      <Header key={'5'} initial footer>
+        <FooterBlock key={'1'} onClick={() => setDate(new Date())}>Today</FooterBlock>
+        <FooterBlock key={'2'} deleteBtn ref={deleteRef} onClick={() => deleteInterview(interwiewToDelete)}>Delete</FooterBlock>
       </Header>
     </Frame>
     
--- styled/front/src/StComponents.js
+++ styled/front/src/StComponents.js
@@ -1,35 +1,52 @@
 import styled, { css } from 'styled-components';
+import { devices } from './devices';
 
 const Frame = styled.div`
-    width: 457px;
+    width: 740px;
     height: 100%;
     border: 1px solid lightgrey;
     box-shadow: 2px 2px 2px #eee;
     overflow: hidden;
+    @media only screen and ${devices.md} {
+        width: 90%;
+    }
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        height: 90%;
+    }
 `;
 
 const Column = styled.div`
-    width: 57px;
+    width: 12.4%;
     height: 40px;
 `;
 
 const Header = styled.div`
     font-size: 18px;
     font-weight: bold;
-    padding: 10px 10px 5px 10px;
+    padding: 10px 10px 5px 10px; ${(props) => (props.initial && '10px 20px 5px 20px') || '10px 10px 5px 10px'};
     display: flex;
     align-items: center;
     justify-content: space-between;
     background-color: ${(props) => (props.initial && '#ffffff') || '#f5f6fa'};
-    height: ${(props) => (props.initial && '9%') || '40px'}
+    height: ${(props) => (props.initial && '9%') || '6%'};
+    background-color: ${(props) => (props.footer && '#f5f6fa')};
 `;
 
 const Button = styled.div`
     cursor: pointer;
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: 28px;
+    }
+`;
+
+const Months = styled.div`
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: 28px;
+    }
 `;
 
 const Body = styled.div`
-    width: ${(props) => (props.initial && '400px') || '100%'};
+    width: ${(props) => (props.initial && '87.6%') || '100%'};
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
@@ -41,7 +58,7 @@ const Hours = styled.div`
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
-    width: 14.2%;
+    width: 12.4%;
     height: 100%;
 `
 
@@ -49,14 +66,14 @@ const BodyWork = styled.div`
     display: flex;
     justify-content: space-between;
     flex-wrap: wrap;
-    width: 85.8%;
+    width: 87.6%;
     height: 100%;
     
 `;
 
 const Day = styled.div`
     width: 14.2%;
-    height: 40px;
+    height: 7%;
     display: flex;
     align-items: center;
     justify-content: center;
@@ -64,25 +81,52 @@ const Day = styled.div`
     background-color: ${(props) => (props.today && 'red' || 'transparent')};
     border-radius: ${(props) => (props.today && '50%' || '0%')};
     color: ${(props) => (props.today && '#ffffff' || '#000000')};
+    font-size: ${(props) => (props.weekday && '12px' || '18px')};
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: ${(props) => (props.weekday && '17px' || '24px')};
+    }
 `;
 
 const WorkBlock = styled.div`
     width: ${(props) => (props.hours && '100%') || '14.2%'};
-    height: 50px;
+    height: 8%;
     display: flex;
     align-items: center;
     justify-content: center;
     cursor: pointer;
+    font-size: 18px;
     padding: ${(props) => (props.hours && '0') || '2px'};
+    color: ${(props) => (props.hours && '#cdcefd') || 'black'};
     background-color: #ffffff;  
     background-clip: ${(props) => (props.hours && 'border-box') || 'content-box'};
     border: ${(props) => (props.hours && 'none') || '0.5px solid #f5f6fa'};
     transform: ${(props) => (props.hours && 'translateY(50%)') || 'translateY(0)'};
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: 28px;
+        border: ${(props) => (props.hours && 'none') || '1px solid #e3e4fd'};
+        color: ${(props) => (props.hours && '#bfc0fe') || 'black'};
+    }
 `
+
+const HeaderBlock = styled.div`
+    cursor: pointer;
+    font-size: ${(props) => (props.plus && '24px' || '18px')};
+    color: ${(props) => (props.plus && 'red') || 'black'};
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: ${(props) => (props.plus && '38px' || '28px')};
+    }
+`
+
 const FooterBlock = styled.div`
     display: ${(props) => (props.deleteBtn && 'none') || 'flex'};
+    cursor: pointer;
+    color: red;
+    font-size: 18px;
+    @media only screen and (min-device-width: 320px) and (max-device-height: 812px) {
+        font-size: 28px;
+    }
 `
 
 
 
-export {Frame, Day, Header, Button, Body, Column, WorkBlock, Hours, BodyWork, FooterBlock};
+export {Frame, Day, Header, Button, Body, Column, WorkBlock, Hours, BodyWork, FooterBlock, HeaderBlock, Months};
